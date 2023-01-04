3c55ffa5f351c533416803026220fafdea06cd9f
sujan <dev.mern.sujan@gmail.com>
2023-01-04 21:49:29 +0600

    login registration e header & footer show off korlam

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
 import useTitle from "../../../Hooks/useTitle";
 import Exercise from "../../../Components/Home/Exercise/Exercise";
+// import Header from "../Components/Shared/Header/Header";
+import Header from "../../Shared/Header/Header.js";
+import Footer from "../../Shared/Footer/Footer.js";
 
 const Home = () => {
   useTitle("Home");
   return (
     <div>
+      <Header></Header>
       <Exercise></Exercise>
+      <Footer></Footer>
     </div>
   );
 };
--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../Components/Shared/Footer/Footer";
-import Header from "../Components/Shared/Header/Header";
+// import Footer from "../Components/Shared/Footer/Footer";
+// import Header from "../Components/Shared/Header/Header";
 
 const Main = () => {
   return (
     <div>
-      <Header></Header>
+      {/* <Header></Header> */}
       <Outlet></Outlet>
-      <Footer></Footer>
+      {/* <Footer></Footer> */}
     </div>
   );
 };
